5739c6a0fd93341c590ec65d7a9a7296a55b54f7
Mr_H <he-jiajun@sjtu.edu.cn>
2025-07-01 11:48:42 +0800

    book2-image10

--- src/perlin.rs
+++ src/perlin.rs
@@ -35,11 +35,24 @@ impl Default for Perlin {
 
 impl Perlin {
     pub fn noise(&self, p: Point3) -> f64 {
-        let i = (((4.0 * p.x()) as i32) & 255) as usize;
-        let j = (((4.0 * p.y()) as i32) & 255) as usize;
-        let k = (((4.0 * p.z()) as i32) & 255) as usize;
-
-        self.ranfloat[self.perm_x[i] as usize ^ self.perm_y[j] as usize ^ self.perm_z[k] as usize]
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
+        let i = p.x().floor() as i32;
+        let j = p.y().floor() as i32;
+        let k = p.z().floor() as i32;
+        let mut c = [[[0.0; 2]; 2]; 2];
+        (0..2).for_each(|di| {
+            (0..2).for_each(|dj| {
+                (0..2).for_each(|dk| {
+                    c[di][dj][dk] = self.ranfloat[self.perm_x[((i + di as i32) & 255) as usize]
+                        as usize
+                        ^ self.perm_y[((j + dj as i32) & 255) as usize] as usize
+                        ^ self.perm_z[((k + dk as i32) & 255) as usize] as usize];
+                })
+            })
+        });
+        Self::trilinear_interp(&c, u, v, w)
     }
 
     fn perlin_generate_perm(p: &mut [i32; POINT_COUNT]) {
@@ -54,5 +67,20 @@ impl Perlin {
             let target = rtweekend::random_int(0, i as i32) as usize;
             p.swap(i, target);
         }
+    }
+
+    fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        (0..2).for_each(|i| {
+            (0..2).for_each(|j| {
+                (0..2).for_each(|k| {
+                    accum += (i as f64 * u + (1 - i) as f64 * (1.0 - u))
+                        * (j as f64 * v + (1 - j) as f64 * (1.0 - v))
+                        * (k as f64 * w + (1 - k) as f64 * (1.0 - w))
+                        * c[i][j][k];
+                })
+            })
+        });
+        accum
     }
 }
