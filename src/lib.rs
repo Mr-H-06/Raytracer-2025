35477ad5aceb1b807560a4073b41b561ac3789a2
Mr_H <he-jiajun@sjtu.edu.cn>
2025-07-04 15:03:48 +0800

    book3-image9

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod hittable_list;
 pub mod interval;
 pub mod material;
 pub mod onb;
+pub mod pdf;
 pub mod perlin;
 pub mod quad;
 pub mod ray;
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ pub mod hittable_list;
 pub mod interval;
 pub mod material;
 pub mod onb;
+pub mod pdf;
 pub mod perlin;
 pub mod quad;
 pub mod ray;
@@ -328,7 +329,7 @@ fn cornell_box() {
 
     cam.aspect_ratio = 1.0;
     cam.image_width = 600;
-    cam.samples_per_pixel = 10;
+    cam.samples_per_pixel = 1000;
     cam.max_depth = 50;
     cam.background = Color::default();
 
